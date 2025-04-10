2e2eccbffced8528acadd24409968977460994e2
{dronreef2} <202001579176@alunos.estacio.br>
2025-04-10 15:50:01 -0300

    soma dobrada

--- src/implementacao/2-soma-dobrada.ts
+++ src/implementacao/2-soma-dobrada.ts
@@ -1,4 +1,19 @@
 function somaDobrada(umValor: number, outroValor: number) {
-    return null;
+
+    if (umValor < 1 || outroValor < 1) {
+        return -1;
+    }
+    
+    
+    const soma = umValor + outroValor;
+    
+    
+    if (umValor === outroValor) {
+        return soma * 2; 
+    }
+    
+    
+    return soma;
 }
+
 module.exports = somaDobrada;
